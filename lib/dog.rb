fde3b3c5f14b06e714f3917f0d2aacb7481da48e
Steven Perfors <stevecanbuild@gmail.com>
2020-03-02 22:58:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,11 +3,17 @@ class Dog
   
   @@all = []
   
-  def initialize
-    @@all << self
+  def initialize(name)
+    @name = name 
+    save 
   end
   
   def self.all
+    @@all 
+  end
+  
+  def save 
+    @@all << self
   end
   
 end
